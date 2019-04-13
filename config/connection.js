3a5d2148bfa4817970d631342ebeb51be0be2ee3
ksknight <korvette_00Whotmail.com>
2019-04-12 22:49:48 -0600

    update code

--- config/connection.js
+++ config/connection.js
@@ -3,25 +3,20 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection({process.env.JAWSDB_URL);
-  } else {
-    connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "hacktheplanet",
-      database: "todoagain_db"
-    });
-    };
-
-
-  // Make connection.
-  connection.connect(function (err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
+  connection = mysql.createConnection({ process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: "8889",
+    database: "q4xrahr97pua8ns7"
   });
+};
 
-  // Export connection for our ORM to use.
-  module.exports = connection;
+
+// Make connection.
+connection.connect();
+
+// Export connection for our ORM to use.
+module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,3 @@
-### Schema
-
 CREATE DATABASE burger_db;
 USE burger_db;
 
@@ -10,3 +8,20 @@ CREATE TABLE burger
 	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+
+
+DROP DATABASE if EXISTS q4xrahr97pua8ns7;
+CREATE DATABASE q4xrahr97pua8ns7;
+USE q4xrahr97pua8ns7;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM burger;
